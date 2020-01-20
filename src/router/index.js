a7201b01a06d07a213599a0efb675b9c4a5973c9
班班 <@qq.com>
2020-01-20 11:26:35 +0800

    登录模块-整合路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,14 @@
 import VueRouter from "vue-router"
 import Vue from "vue"
+import login from "../views/login/login.vue"
+
 Vue.use(VueRouter)
 const router = new VueRouter({
-    routes:[]
+    routes:[
+        {
+            path:"/login",
+            component:login
+        }
+    ]
 })
 export default router
